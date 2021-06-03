5cc15bc53f8c20293f3c8e1c09a2800d740549f1
linG <3137168214@qq.com>
2021-06-04 00:07:27 +0800

    填加基本框架

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,31 @@
 module.exports = {
-  title: 'My Code Life',
-  description: 'My Code Life 记录了我的程序员生涯中学到的各种知识,遇到的各种问题',
+  title: 'Yolo Cloud',
+  description: 'Yolo Cloud 记录了我技术成长中学到的各种知识,遇到的各种问题',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
   themeConfig: {
+    activeHeaderLinks: false,
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '主页', link: '/' },
       { text: 'GitHub', link: 'https://github.com/linG5821' },
+    ],
+    sidebar: [
+      {
+        title: '机器环境',
+        path: '/machine/',
+        sidebarDepth: 1,
+        children: [
+          { title: 'Windows', path: '/machine/windows/' },
+          { title: 'Mac', path: '/machine/mac/' },
+          { title: 'Linux', path: '/machine/linux/' },
+        ]
+      },
+      {
+        title: '网络编程',
+        path: '/network/',
+        sidebarDepth: 1
+      },
     ]
   }
 }
